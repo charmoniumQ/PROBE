77b2ffc6398c9ac713023bd8e5bfdb38d5fab431
Samuel Grayson <sam@samgrayson.me>
2024-03-21 00:51:45 -0500

    Minor changes

--- benchmark/data_minimizer.py
+++ benchmark/data_minimizer.py
@@ -5,25 +5,42 @@ import urllib.parse
 import pathlib
 import pandas
 import pickle
+import random
 import charmonium.time_block as ch_time_block
 from workloads import WORKLOAD_GROUPS
-from prov_collectors import PROV_COLLECTOR_GROUPS
+from prov_collectors import PROV_COLLECTOR_GROUPS, ProvOperation
 import psutil
 
 proc = psutil.Process()
 
 cache_dir = pathlib.Path(".cache")
 
-prov_collectors = PROV_COLLECTOR_GROUPS["working"]
-workloads = WORKLOAD_GROUPS["working-low-mem"]
+# prov_collectors = PROV_COLLECTOR_GROUPS["working"]
+# workloads = WORKLOAD_GROUPS["working-low-mem"]
+prov_collectors = PROV_COLLECTOR_GROUPS["fsatrace"]
+workloads = WORKLOAD_GROUPS["working"]
 
-iterations = 3
+iterations = 1
 
 size = 256
 
 seed = 0
 
+rng = random.Random(seed)
+
+def get_overwritten_dependencies(operations: collections.Counter[ProvOperation]) -> set[str]:
+    dependencies = set[str]()
+    overwritten_dependencies = set[str]()
+    for op in operations:
+        if op.type.lower() in {"r", "t", "q"}:
+            dependencies.add(op.target0)
+        elif op.type.lower() in {"w", "m", "d"}:
+            if op.target0 in dependencies:
+                overwritten_dependencies.add(op.target0)
+    return overwritten_dependencies
+
 records = []
+workloads_with_overwritten_deps = set[str]()
 for prov_collector in prov_collectors:
     for workload in tqdm.tqdm(workloads, desc=f"{prov_collector.name} workloads"):
         for iteration in range(iterations):
@@ -35,6 +52,16 @@ for prov_collector in prov_collectors:
                 ]))).with_suffix(".pkl")
                 with key.open("rb") as file_obj:
                     stats = pickle.load(file_obj)
+                overwritten_deps = get_overwritten_dependencies(stats.operations)
+                if overwritten_deps:
+                    workloads_with_overwritten_deps.add(workload.name)
+                    print(workload.name, "overwritten deps:")
+                    k = 10
+                    if len(overwritten_deps) > k:
+                        test_deps = rng.sample(list(overwritten_deps), 10)
+                    else:
+                        test_deps = list(overwritten_deps)
+                    print("\n".join(test_deps))
                 record = {
                     "collector": prov_collector.name,
                     "collector_method": prov_collector.method,
@@ -51,12 +78,12 @@ for prov_collector in prov_collectors:
                     #     (op.target0 for op in stats.operations),
                     #     (op.target1 for op in stats.operations),
                     # )),
+                    "raw-dependencies": overwritten_deps,
                     "op_type_counts": collections.Counter(
                         op.type for op in stats.operations
                     ),
                 }
                 records.append(record)
-                print(proc.memory_info().rss // 1024 // 1024, "MiB")
 
 results_df = (
     pandas.DataFrame.from_records(list(filter(bool, records)))
@@ -76,3 +103,4 @@ key = (cache_dir / ("results_" + "_".join([
     str(seed),
 ]) + ".pkl"))
 key.write_bytes(pickle.dumps(results_df))
+print(workloads_with_overwritten_deps, "workloads with overwritten deps")
--- docs/benchmark_suite/main.md
+++
@@ -1,9 +0,0 @@
-\begin{tabular}{p{2in}}
-test
-\end{tabular}
-
-# Tables
-
-\begin{tabular}{p{2\linewidth}}
-\end{tabular}
-
