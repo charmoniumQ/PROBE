dd1a8799674887cd112b5a888cd078935fbcd709
Jenna Fligor <jenna@fligor.net>
2024-08-24 07:40:35 -0500

    rename frontend and nix cleanup

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     flake-utils,
     rust-overlay,
     ...
-  }@inputs: let
+  } @ inputs: let
     supported-systems = [
       "x86_64-linux"
       "i686-linux"
@@ -37,64 +37,68 @@
       "armv7l-linux"
     ];
   in
-    flake-utils.lib.eachSystem supported-systems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [(import rust-overlay)];
-      };
-      python = pkgs.python312;
-      frontend = (import ./probe_src/probe_frontend/frontend.nix) ({ inherit system pkgs python; } // inputs);
-    in {
-        packages = rec {
-          probe-bundled = let
-            # libprobe is a "private" package
-            # It is only used in probe-bundled
-            # TODO: The only public package should probably be probe-bundled and probe-py.
-            libprobe = pkgs.stdenv.mkDerivation rec {
-              pname = "libprobe";
+    flake-utils.lib.eachSystem supported-systems (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+        python = pkgs.python312;
+        frontend = (import ./probe_src/frontend/frontend.nix) ({inherit system pkgs python;} // inputs);
+      in {
+        packages =
+          rec {
+            probe-bundled = let
+              # libprobe is a "private" package
+              # It is only used in probe-bundled
+              # TODO: The only public package should probably be probe-bundled and probe-py.
+              libprobe = pkgs.stdenv.mkDerivation rec {
+                pname = "libprobe";
+                version = "0.1.0";
+                src = ./probe_src/libprobe;
+                makeFlags = ["INSTALL_PREFIX=$(out)" "SOURCE_VERSION=${version}"];
+                buildInputs = [
+                  (pkgs.python312.withPackages (pypkgs: [
+                    pypkgs.pycparser
+                  ]))
+                ];
+              };
+            in
+              pkgs.stdenv.mkDerivation rec {
+                pname = "probe-bundled";
+                version = "0.1.0";
+                dontUnpack = true;
+                dontBuild = true;
+                nativeBuildInputs = [pkgs.makeWrapper];
+                installPhase = ''
+                  mkdir $out $out/bin
+                  makeWrapper \
+                    ${self.packages.${system}.probe-cli}/bin/probe \
+                    $out/bin/probe \
+                    --set __PROBE_LIB ${libprobe}/lib
+                '';
+              };
+            probe-py-manual = python.pkgs.buildPythonPackage rec {
+              pname = "probe_py.manual";
               version = "0.1.0";
-              src = ./probe_src/libprobe;
-              makeFlags = [ "INSTALL_PREFIX=$(out)" "SOURCE_VERSION=${version}" ];
-              buildInputs = [
-                (pkgs.python312.withPackages (pypkgs: [
-                  pypkgs.pycparser
-                ]))
+              pyproject = true;
+              build-system = [
+                python.pkgs.flit-core
               ];
+              src = ./probe_src/python;
+              propagatedBuildInputs = [
+                self.packages.${system}.probe-py-generated
+                python.pkgs.networkx
+                python.pkgs.pygraphviz
+                python.pkgs.pydot
+                python.pkgs.rich
+                python.pkgs.typer
+              ];
+              pythonImportsCheck = [pname];
             };
-          in pkgs.stdenv.mkDerivation rec {
-            pname = "probe-bundled";
-            version = "0.1.0";
-            dontUnpack = true;
-            dontBuild = true;
-            nativeBuildInputs = [ pkgs.makeWrapper ];
-            installPhase = ''
-              mkdir $out $out/bin
-              makeWrapper \
-                ${self.packages.${system}.probe-cli}/bin/probe \
-                $out/bin/probe \
-                --set __PROBE_LIB ${libprobe}/lib
-            '';
-          };
-          probe-py-manual = python.pkgs.buildPythonPackage rec {
-            pname = "probe_py.manual";
-            version = "0.1.0";
-            pyproject = true;
-            build-system = [
-              python.pkgs.flit-core
-            ];
-            src = ./probe_src/python;
-            propagatedBuildInputs = [
-              self.packages.${system}.probe-py-generated
-              python.pkgs.networkx
-              python.pkgs.pygraphviz
-              python.pkgs.pydot
-              python.pkgs.rich
-              python.pkgs.typer
-            ];
-            pythonImportsCheck = [ pname ];
-          };
-          default = probe-bundled;
-        } // frontend.packages;
+            default = probe-bundled;
+          }
+          // frontend.packages;
         # TODO: Run pytest tests in Nix checks
         checks = self.packages.${system} // frontend.checks;
         devShells = {
@@ -105,7 +109,8 @@
               popd
             '';
             buildInputs =
-              oldAttrs.buildInputs ++ [
+              oldAttrs.buildInputs
+              ++ [
                 (python.withPackages (pypkgs: [
                   # probe_py.manual runtime requirements
                   pypkgs.networkx
@@ -135,20 +140,12 @@
                 pkgs.black
                 pkgs.ruff
               ]
-              ++ (
-                # gdb broken on apple silicon
-                if system != "aarch64-darwin"
-                then [pkgs.gdb]
-                else []
-              )
-              ++ (
-                # while xdot isn't marked as linux only, it has a dependency (xvfb-run) that is
-                if builtins.elem system pkgs.lib.platforms.linux
-                then [pkgs.xdot]
-                else []
-              );
+              # gdb broken on apple silicon
+              ++ pkgs.lib.lists.optional (system != "aarch64-darwin") pkgs.gdb
+              # while xdot isn't marked as linux only, it has a dependency (xvfb-run) that is
+              ++ pkgs.lib.lists.optional (builtins.elem system pkgs.lib.platforms.linux) pkgs.xdot;
           });
         };
-       }
+      }
     );
 }
