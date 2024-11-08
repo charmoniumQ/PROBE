cf3bda8df279744ed49397c89a2c7c60a70661a1
Saleha Muzammil <84681153+saleha-muzammil@users.noreply.github.com>
2024-11-08 22:49:03 +0500

    Update test_program.c

--- test_program.c
+++ test_program.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main() {
     FILE *file = fopen("flake.nix", "r");
@@ -12,6 +14,18 @@ int main() {
     } else {
         perror("fopen");
         return EXIT_FAILURE;
+    }
+    int fd = open("flake.nix", O_RDONLY);
+    if (fd != -1) {
+        char buffer[100];
+        ssize_t bytes = read(fd, buffer, sizeof(buffer)-1);
+        if (bytes > 0) {
+            buffer[bytes] = '\0';
+            printf("Read from open: %s\n", buffer);
+        }
+        close(fd);
+    } else {
+        perror("open");
     }
     return EXIT_SUCCESS;
 }
