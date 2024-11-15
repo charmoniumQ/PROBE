69de7a932f53cee2cc5df98c69079f222f705526
shofiya <shofiyabootwala@gmail.com>
2024-11-16 01:11:29 +0530

    feat: created schema for tables

---
+++ probe_src/python/probe_py/manual/persistent_provenance_db.py
@@ -0,0 +1,55 @@
+from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP, PrimaryKeyConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import xdg_base_dirs
+
+database_path = xdg_base_dirs.xdg_data_home() / "probe_log.db"
+print(database_path)
+engine = create_engine(f'sqlite:///{database_path}', echo=True)
+Base = declarative_base()
+
+
+class ProcessThatWrites(Base):
+    __tablename__ = 'process_that_writes'
+
+    inode = Column(Integer)
+    process_id = Column(Integer)
+    device_major = Column(Integer)
+    device_minor = Column(Integer)
+    host = Column(String)
+    path = Column(String)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('inode', 'process_id'),  # Composite primary key
+    )
+
+
+class Process(Base):
+    __tablename__ = 'process'
+
+    id = Column(Integer, primary_key=True)
+    cmd = Column(String)
+    time = Column(TIMESTAMP)
+
+
+class ProcessInputs(Base):
+    __tablename__ = 'process_inputs'
+
+    inode = Column(Integer)
+    process_id = Column(Integer)
+    device_major = Column(Integer)
+    device_minor = Column(Integer)
+    host = Column(String)
+    path = Column(String)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('inode', 'process_id'),
+    )
+
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+print("Tables for persistent provenance created successfully.")
