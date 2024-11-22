e6272341da671d149d57f7de97e0332910ebd0c3
shofiya <shofiyabootwala@gmail.com>
2024-11-23 03:53:03 +0530

    made changes to solve the precommit errors

--- probe_src/python/probe_py/manual/persistent_provenance_db.py
+++ probe_src/python/probe_py/manual/persistent_provenance_db.py
@@ -1,13 +1,15 @@
-from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP, PrimaryKeyConstraint
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine, DateTime
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy.engine import Engine
 import xdg_base_dirs
 import pathlib
+from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
 
 _engine = None
-def get_engine():
+def get_engine()->Engine:
     global _engine
     if _engine is None:
         home = pathlib.Path(xdg_base_dirs.xdg_data_home())
@@ -20,36 +22,38 @@ def get_engine():
 
 class ProcessThatWrites(Base):
     __tablename__ = 'process_that_writes'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    inode = Column(Integer)
-    process_id = Column(Integer)
-    device_major = Column(Integer)
-    device_minor = Column(Integer)
-    host = Column(Integer)
-    path = Column(String)
-    mtime = Column(Integer)
-    size = Column(Integer)
+
+    id: Mapped[int] = mapped_column(primary_key=True, auto_increment=True)
+    inode: Mapped[int]
+    process_id: Mapped[int]
+    device_major: Mapped[int]
+    device_minor: Mapped[int]
+    host: Mapped[int]
+    path: Mapped[str]
+    mtime: Mapped[int]
+    size: Mapped[int]
 
 
 class Process(Base):
     __tablename__ = 'process'
 
-    process_id = Column(Integer, primary_key=True)
-    parent_process_id = Column(Integer, nullable=True)
-    cmd = Column(String)
-    time = Column(TIMESTAMP)
+    process_id: Mapped[int] = mapped_column(primary_key=True)
+    parent_process_id: Mapped[int]
+    cmd: Mapped[str]
+    time: Mapped[datetime] = mapped_column(DateTime)
 
 
 class ProcessInputs(Base):
     __tablename__ = 'process_inputs'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    inode = Column(Integer)
-    process_id = Column(Integer)
-    device_major = Column(Integer)
-    device_minor = Column(Integer)
-    host = Column(Integer)
-    path = Column(String)
-    mtime = Column(Integer)
-    size = Column(Integer)
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    inode: Mapped[int]
+    process_id: Mapped[int]
+    device_major: Mapped[int]
+    device_minor: Mapped[int]
+    host: Mapped[int]
+    path: Mapped[str]
+    mtime: Mapped[int]
+    size: Mapped[int]
 
 print("Tables for persistent provenance created successfully.")
