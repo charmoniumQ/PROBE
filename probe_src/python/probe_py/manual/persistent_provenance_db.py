339234f815f97095a5752f27cfaee1dfb4d53269
shofiya <shofiyabootwala@gmail.com>
2024-11-23 03:25:33 +0530

    made changes to the schema

--- probe_src/python/probe_py/manual/persistent_provenance_db.py
+++ probe_src/python/probe_py/manual/persistent_provenance_db.py
@@ -1,27 +1,34 @@
 from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP, PrimaryKeyConstraint
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import xdg_base_dirs
-
-database_path = xdg_base_dirs.xdg_data_home() / "probe_log.db"
-engine = create_engine(f'sqlite:///{database_path}', echo=True)
+import pathlib
 
 class Base(DeclarativeBase):
     pass
 
+_engine = None
+def get_engine():
+    global _engine
+    if _engine is None:
+        home = pathlib.Path(xdg_base_dirs.xdg_data_home())
+        home.mkdir(parents=True, exist_ok=True)
+        database_path = home / "probe_log.db"
+
+        _engine = create_engine(f'sqlite:///{database_path}', echo=True)
+        Base.metadata.create_all(_engine)
+    return _engine
 
 class ProcessThatWrites(Base):
     __tablename__ = 'process_that_writes'
-
+    id = Column(Integer, primary_key=True, autoincrement=True)
     inode = Column(Integer)
     process_id = Column(Integer)
     device_major = Column(Integer)
     device_minor = Column(Integer)
-    host = Column(String)
+    host = Column(Integer)
     path = Column(String)
-
-    __table_args__ = (
-        PrimaryKeyConstraint('inode', 'process_id'),  # Composite primary key
-    )
+    mtime = Column(Integer)
+    size = Column(Integer)
 
 
 class Process(Base):
@@ -35,22 +42,14 @@ class Process(Base):
 
 class ProcessInputs(Base):
     __tablename__ = 'process_inputs'
-
+    id = Column(Integer, primary_key=True, autoincrement=True)
     inode = Column(Integer)
     process_id = Column(Integer)
     device_major = Column(Integer)
     device_minor = Column(Integer)
-    host = Column(String)
+    host = Column(Integer)
     path = Column(String)
-
-    __table_args__ = (
-        PrimaryKeyConstraint('inode', 'process_id'),
-    )
-
-
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
+    mtime = Column(Integer)
+    size = Column(Integer)
 
 print("Tables for persistent provenance created successfully.")
