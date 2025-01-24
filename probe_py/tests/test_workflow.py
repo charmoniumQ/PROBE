049d86c406a0b30f2d37450de71f4349dd2d1c28
shofiya <shofiyabootwala@gmail.com>
2025-01-24 20:35:22 +0530

    fix: changed test file to use the modified FileNode object

--- probe_py/tests/test_workflow.py
+++ probe_py/tests/test_workflow.py
@@ -2,7 +2,7 @@ import re
 import pytest
 import pathlib
 import networkx as nx  # type: ignore
-from probe_py.analysis import FileNode, ProcessNode, InodeOnDevice
+from probe_py.analysis import FileNode, ProcessNode, InodeOnDevice, FileVersion
 from probe_py.workflows import NextflowGenerator
 
 
@@ -18,8 +18,9 @@ def test_dataflow_graph_to_nextflow_script() -> None:
     b_file_path.write_text("This is A.txt")
 
     dataflow_graph = nx.DiGraph()
-    A = FileNode(InodeOnDevice(0,0,0), (0, 0), "A.txt")
-    B = FileNode(InodeOnDevice(0,0,1), (0, 0), "B.txt")
+
+    A = FileNode(InodeOnDevice(0,0,0), FileVersion(0, 0), "A.txt")
+    B = FileNode(InodeOnDevice(0,0,1), FileVersion(0, 0), "B.txt")
     W = ProcessNode(0, ("cp", "A.txt", "B.txt"))
     dataflow_graph.add_nodes_from([A, B], color="red")
     dataflow_graph.add_nodes_from([W], color="blue")
@@ -57,10 +58,10 @@ workflow {
     expected_script = re.sub(r'process_\d+', 'process_*', expected_script)
     assert script == expected_script
 
-    A = FileNode(InodeOnDevice(0,0,0), (0, 0), "A.txt")
-    B0 = FileNode(InodeOnDevice(0,0,1), (0, 0), "B.txt")
-    B1 = FileNode(InodeOnDevice(0,0,1), (1, 0), "B.txt")
-    C = FileNode(InodeOnDevice(0,0,3), (0, 0), "C.txt")
+    A = FileNode(InodeOnDevice(0,0,0), FileVersion(0, 0), "A.txt")
+    B0 = FileNode(InodeOnDevice(0,0,1), FileVersion(0, 0), "B.txt")
+    B1 = FileNode(InodeOnDevice(0,0,1), FileVersion(1, 0), "B.txt")
+    C = FileNode(InodeOnDevice(0,0,3), FileVersion(0, 0), "C.txt")
     W = ProcessNode(0,("cp", "A.txt", "B.txt"))
     X = ProcessNode(1,("sed", "s/foo/bar/g", "-i", "B.txt"))
     # Note, the filename in FileNode will not always appear in the cmd of ProcessNode!
