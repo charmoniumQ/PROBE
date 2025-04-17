335d47842bf1140a331ca628251a192a11c42ba0
Samuel Grayson <sam@samgrayson.me>
2025-04-17 17:49:58 -0500

    Fix stuff

--- cli-wrapper/cli/src/record.rs
+++ cli-wrapper/cli/src/record.rs
@@ -161,51 +161,43 @@ impl Recorder {
             ""
         };
 
+        /* We start `probe __exec $cmd` instead of `$cmd`
+         * This is because PROBE is not able to capture the arguments of the very first process, but it does capture the arguments of any subsequent exec(...).
+         * Therefore, the "root process" is env, and the user's $cmd is exec(...)-ed.
+         * We could change this by adding argv and environ to InitProcessOp, but I think this solution is more elegant.
+         * Since the root process has special quirks, it should not be user's `$cmd`.
+         * */
         let mut child = if self.gdb {
-            let dir_env = concat_osstrings([
-                OsString::from("--init-eval-command=set environment "),
-                OsString::from(probe_headers::PROBE_DIR_VAR),
-                OsString::from("="),
-                OsString::from(&self.output.path()),
-            ]);
-            let preload_env = concat_osstrings([
-                OsString::from("--init-eval-command=set environment "),
-                OsString::from(probe_headers::LD_PRELOAD_VAR),
-                OsString::from("="),
-                ld_preload,
-            ]);
-            let copy_files_env = concat_osstrings([
-                OsString::from("--init-eval-command=set environment "),
-                OsString::from(probe_headers::PROBE_COPY_FILES_VAR),
-                OsString::from("="),
-                OsString::from(copy_files_string),
-            ]);
-
             std::process::Command::new("gdb")
-                .arg(dir_env)
-                .arg(preload_env)
-                .arg(copy_files_env)
+                .arg(concat_osstrings([
+                    OsString::from("--init-eval-command=set environment "),
+                    OsString::from(probe_headers::PROBE_DIR_VAR),
+                    OsString::from("="),
+                    OsString::from(&self.output.path()),
+                ]))
+                .arg(concat_osstrings([
+                    OsString::from("--init-eval-command=set environment "),
+                    OsString::from(probe_headers::LD_PRELOAD_VAR),
+                    OsString::from("="),
+                    ld_preload,
+                ]))
+                .arg(concat_osstrings([
+                    OsString::from("--init-eval-command=set environment "),
+                    OsString::from(probe_headers::PROBE_COPY_FILES_VAR),
+                    OsString::from("="),
+                    OsString::from(copy_files_string),
+                ]))
                 .arg("--init-eval-command=set environment LD_DEBUG=all")
                 .arg("--args")
                 .arg(self_bin)
                 .arg("__exec")
                 .args(&self.cmd)
-                .env_remove("PROBE_LIB")
-                .env_remove("PROBE_LOG")
                 .spawn()
                 .wrap_err("Failed to launch gdb")?
         } else {
-            /* We start `probe __exec $cmd` instead of `$cmd`
-             * This is because PROBE is not able to capture the arguments of the very first process, but it does capture the arguments of any subsequent exec(...).
-             * Therefore, the "root process" is env, and the user's $cmd is exec(...)-ed.
-             * We could change this by adding argv and environ to InitProcessOp, but I think this solution is more elegant.
-             * Since the root process has special quirks, it should not be user's `$cmd`.
-             * */
             std::process::Command::new(self_bin)
                 .arg("__exec")
                 .args(self.cmd)
-                .env_remove("PROBE_LIB")
-                .env_remove("PROBE_LOG")
                 .env(probe_headers::PROBE_COPY_FILES_VAR, copy_files_string)
                 .env(
                     probe_headers::PROBE_DIR_VAR,
