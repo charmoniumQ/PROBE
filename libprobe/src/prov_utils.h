690fac2825b52b8e6a5246666aafaa05e618d48f
Samuel Grayson <sam@samgrayson.me>
2025-08-07 23:22:47 -0500

    Fix stuff

--- libprobe/src/prov_utils.h
+++ libprobe/src/prov_utils.h
@@ -4,12 +4,8 @@
 
 #include "util.h"    // for BORROWED
 #include <stdbool.h> // for bool
-// IWYU pragma: no_include "libprobe/prov_ops.h" for Op (ptr only), StatResult (ptr only)
-// IWYU pragma: no_include "/build/libprobe/include/libprobe/prov_ops.h"
+#include "include/libprobe/prov_ops.h"
 
-struct Op;         // IWYU pragma: keep
-struct StatResult; // IWYU pragma: keep
-struct my_rusage;  // IWYU pragma: keep
 struct rusage;
 struct stat;
 struct statx;
