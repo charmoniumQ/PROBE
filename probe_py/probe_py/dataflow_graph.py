2512f401de18c9ec237dbb7ee45054aeb09431ee
Samuel Grayson <sam@samgrayson.me>
2025-08-20 17:33:33 -0500

    Guard against edge case

--- probe_py/probe_py/dataflow_graph.py
+++ probe_py/probe_py/dataflow_graph.py
@@ -46,7 +46,7 @@ def accesses_to_dataflow_graph(
         WRITING = enum.auto()
 
     parent_pid = probe_log.get_parent_pid_map()
-    pid_to_state = collections.defaultdict[ptypes.Pid, PidState](lambda: PidState.READING)
+    pid_to_state = dict[ptypes.Pid, PidState]()
     last_op_in_process = dict[ptypes.Pid, ptypes.OpQuint]()
     inode_to_version = collections.defaultdict[ptypes.Inode, int](lambda: 0)
     inode_to_paths = collections.defaultdict[ptypes.Inode, set[pathlib.Path]](set)
@@ -67,6 +67,8 @@ def accesses_to_dataflow_graph(
         last_op_in_process[quad.pid] = quint
 
     def ensure_state(quad: ptypes.OpQuad, desired_state: PidState) -> ptypes.OpQuint:
+        if quad.pid not in pid_to_state:
+            add_quad(quad, "init")
         if desired_state == PidState.WRITING and pid_to_state[quad.pid] == PidState.READING:
             # Reading -> writing for free
             pid_to_state[quad.pid] = PidState.WRITING
