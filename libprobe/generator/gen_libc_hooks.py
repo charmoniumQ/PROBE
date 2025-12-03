9e727a95a1d52fc1a17902f1544c62e2390fa6cd
Samuel Grayson <sam@samgrayson.me>
2025-12-03 17:43:08 -0600

    Start C multilevel table

--- libprobe/generator/gen_libc_hooks.py
+++ libprobe/generator/gen_libc_hooks.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 import dataclasses
-import pycparser  # type: ignore
 import pycparser.c_generator  # type: ignore
 import typing
 import itertools
 import pathlib
+from pycparser_types import CGenerator, Assignment, Compound, Decl, Node, ID, TypeDecl, IdentifierType, PtrDecl, FuncDecl, ParamList
 
 
 # Intercept libc functions
@@ -22,71 +22,6 @@ def expect_type(typ: type[_T], data: typing.Any) -> _T:
     if not isinstance(data, typ):
         raise TypeError(f"Expected type {typ} for {data}")
     return data
-
-
-if typing.TYPE_CHECKING:
-    class CGenerator:
-        def _parenthesize_if(self, n: Node, condition: typing.Callable[[Node], bool]) -> str: ...
-        def _generate_decl(self, n: pycparser.c_ast.Node) -> str: ...
-        def visit(self, n: pycparser.c_ast.Node | str | list[str]) -> str: ...
-        def _visit_expr(self, n: pycparser.c_ast.Node) -> str: ...
-        def _make_indent(self) -> str: ...
-        indent_level: int
-    class Node:
-        def __iter__(self) -> typing.Iterator[Node]: ...
-    @dataclasses.dataclass
-    class IdentifierType(Node):
-        names: list[str]
-    @dataclasses.dataclass
-    class Assignment(Node):
-        op: str
-        lvalue: Node
-        rvalue: Node
-    @dataclasses.dataclass
-    class Compound(Node):
-        block_items: list[Node]
-    @dataclasses.dataclass
-    class ID(Node):
-        name: str
-    @dataclasses.dataclass
-    class Decl(Node):
-        name: str
-        quals: list[str]
-        align: list[str]
-        storage: list[str]
-        funcspec: list[str]
-        type: TypeDecl | PtrDecl
-        init: Node | None = None
-        bitsize: Node | None = None
-    @dataclasses.dataclass
-    class TypeDecl(Node):
-        declname: str | None
-        quals: list[Node]
-        align: Node | None
-        type: Node
-    @dataclasses.dataclass
-    class FuncDecl(Node):
-        args: ParamList | None
-        type: TypeDecl
-    @dataclasses.dataclass
-    class ParamList(Node):
-        params: list[Decl]
-    @dataclasses.dataclass
-    class PtrDecl(Node):
-        quals: list[str]
-        type: TypeDecl | FuncDecl
-else:
-    CGenerator = pycparser.c_generator.CGenerator
-    Node = pycparser.c_ast.Node
-    IdentifierType = pycparser.c_ast.IdentifierType
-    Assignment = pycparser.c_ast.Assignment
-    Compound = pycparser.c_ast.Compound
-    Decl = pycparser.c_ast.Decl
-    TypeDecl = pycparser.c_ast.TypeDecl
-    ID = pycparser.c_ast.ID
-    FuncDecl = pycparser.c_ast.FuncDecl
-    ParamList = pycparser.c_ast.ParamList
-    PtrDecl = pycparser.c_ast.PtrDecl
 
 
 class GccCGenerator(CGenerator):
---
+++ libprobe/generator/multilevel_table.py
@@ -0,0 +1,123 @@
+import typing
+import itertools
+
+
+def multilevel_table(
+        name: str,
+        bit_lengths: typing.Sequence[int],
+        value_type: str,
+) -> list[str]:
+    n_levels = len(bit_lengths)
+    total_bit_length = sum(bit_lengths)
+    total_bit_length_rounded = (
+        16 if total_bit_length <= 16 else
+        32 if total_bit_length <= 32 else
+        64 if total_bit_length <= 64 else
+        raise_(ValueError("Cannot go bigger than 64 bit_lengths"))
+    )
+
+    # Calculate masks, shifts, and bit_lengths
+    shifts = [
+        total_bit_length_rounded - sum(bit_lengths[:i+1])
+        for i in range(n_levels)
+    ]
+    masks = [
+        ((1 << bit_length) - 1) << shift
+        for bit_length, shift in zip(bit_lengths, shifts)
+    ]
+    lengths = [
+        1 << bit_length
+        for bit_length in bit_lengths
+    ]
+
+    # Assert no two masks overlap
+    for mask0, mask1 in itertools.combinations(masks, 2):
+        assert mask0 & mask1 == 0
+
+    # Assert all masks "add up" to total bit_lengths
+    total = 0
+    for mask in masks:
+        total |= mask
+    assert total == (1 << total_bit_length) - 1
+
+    sizeof_pointer = 16
+    sizeof_one_entry = sum([length * sizeof_pointer for length in lengths])
+
+    # Name, but formatted for a type name
+    type_name = name.capitalize()
+    const_name = name.upper()
+    func_name = name
+    struct_names = [f"_{type_name}Lvl{i}" for i in range(n_levels)]
+    inner_name = "inner"
+    inners_names = [f"inner{i}" for i in range(n_levels)]
+    mask_names = [f"_{const_name}_MASK{i}" for i in range(n_levels)]
+    shift_names = [f"_{const_name}_SHIFT{i}" for i in range(n_levels)]
+
+    last_level = n_levels - 1
+    create_defn = f"{type_name}* {func_name}_create();"
+    create_decl = f"{create_defn[:-1]} {{ return EXPECT_NONNULL(calloc(sizeof(struct {struct_names[0]}), 1)); }}"
+    index_type = f"uint{total_bit_length_rounded}_t"
+    shift_names = [f"_{type_name}_SHIFT{i}" for i in range(n_levels)]
+    mask_names = [f"_{type_name}_MASK{i}" for i in range(n_levels)]
+    get_or_default_decl = f"{value_type} {func_name}_get_or_default(const {type_name}* table, {index_type} index, {value_type} def);"
+    get_or_default_defn = [
+        f"{get_or_default_decl[:-1]} {{",
+        f"  const struct {struct_names[0]}* {inners_names[0]} = table;",
+        *itertools.chain.from_iterable([
+            [
+                f"  const struct {struct_names[i+1]}* {inners_names[i+1]} = {inners_names[i]}->inner[(index & {mask_names[i]}) >> {shift_names[i]}];",
+                f"  if (!{inners_names[i]}) {{ return def; }}",
+            ]
+            for i in range(last_level)
+        ]),
+        f"  {value_type} ret = {inners_names[last_level]}->inner[(index & {mask_names[last_level]}) >> {shift_names[last_level]}];",
+        "  return ret;",
+        "}",
+    ]
+    set_if_empty_decl = f"{value_type} {func_name}_set_if_empty({type_name}* table, {index_type} index, {value_type} value);"
+    set_if_empty_defn = [
+        f"{set_if_empty_decl[:-1]} {{",
+        "  ASSERTF(value != 0 && value != NULL, \"This function only makes sense with a non-zero value. Otherwise, it would appear empty after being set.\");"
+        f"  struct {struct_names[0]}** inner0 = &table;",
+        *itertools.chain.from_iterable([
+            [
+                f"  struct {struct_names[i+1]}** {inners_names[i+1]} = &(*{inners_names[i]})->inner[(index & {mask_names[i]}) >> {shift_names[i]}];",
+                f"  if (!*{inners_names[i+1]}) {{ *{inners_names[i+1]} = EXPECT_NONNULL(calloc(sizeof(struct {struct_names[i+1]}), 1)); }}",
+            ]
+            for i in range(last_level)
+        ]),
+        f"  {value_type}* ret = &(*{inners_names[last_level]})->inner[(index & {mask_names[last_level]}) >> {shift_names[last_level]}];",
+        "  if (!*ret) {{ *ret = value; }}",
+        "  return *ret;",
+        "}",
+    ]
+    return [
+        "#include <stdint.h>",
+        "#include <stdlib.h>",
+        "#define EXPECT_NONNULL(x) ((x))",
+        "#define ASSERTF(x, y)",
+        *[
+            f"#define {mask_names[i]} 0x{mask:0{total_bit_length_rounded // 4}x}"
+            for i, mask in enumerate(masks)
+        ],
+        *[
+            f"#define {shift_names[i]} {shifts[i]}"
+            for i, mask in enumerate(masks)
+        ],
+        "",
+        f"struct {struct_names[last_level]} {{ {value_type} {inner_name} [{lengths[last_level]}]; }};",
+        *[
+            f"struct {struct_names[i]} {{ struct {struct_names[i+1]}* {inner_name} [{lengths[i]}]; }};"
+            for i in range(last_level - 1, -1, -1)
+        ],
+        f"typedef struct {struct_names[0]} {type_name};",
+        f"// sizeof table with one entry = {sizeof_one_entry // 1024}KiB",
+        "",
+        create_decl,
+        *get_or_default_defn,
+        *set_if_empty_defn,
+    ]
+
+
+def raise_(exc: Exception) -> typing.NoReturn:
+    raise exc
