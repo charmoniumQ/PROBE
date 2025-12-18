0ba0b1a98c34fe2be95821c812252710e3cd8640
Samuel Grayson <sam@samgrayson.me>
2025-12-17 20:17:08 -0600

    Add generated inode table

--- libprobe/generator/radix_table.py
+++ libprobe/generator/radix_table.py
@@ -10,44 +10,21 @@ def multilevel_table(
         name: str,
         value_type: str,
         log_lengths: typing.Sequence[int],
+        sizeof_pointer: int = 8,
+        sizeof_value: int = 8,
 ) -> None:
     n_levels = len(log_lengths)
+    last_level = n_levels - 1
     log_length = sum(log_lengths)
     log_length_rounded = (
         16 if log_length <= 16 else
         32 if log_length <= 32 else
         64 if log_length <= 64 else
-        96 if log_length <= 96 else 
+        128 if log_length <= 128 else 
         raise_(ValueError("Cannot go bigger than 64 log_lengths"))
     )
-
-    # Calculate masks, shifts, and log_lengths
-    shifts = [
-        log_length - sum(log_lengths[:i+1])
-        for i in range(n_levels)
-    ]
-    masks = [
-        ((1 << bit_length) - 1) << shift
-        for bit_length, shift in zip(log_lengths, shifts)
-    ]
-    lengths = [
-        1 << bit_length
-        for bit_length in log_lengths
-    ]
-    sizeof_pointer = 16
-    sizeof_one_entry = sum([length * sizeof_pointer for length in lengths])
-
-    # Assert no two masks overlap
-    for mask0, mask1 in itertools.combinations(masks, 2):
-        assert mask0 & mask1 == 0, f"Masks overlap {mask0} {mask1}"
-
-    total = 0
-    for mask in masks:
-        total |= mask
-    assert total == (1 << log_length) - 1, "Masks don't add up:\n" + "\n".join(
-        f"{mask << shift:0{log_length}b}"
-        for mask, shift in zip(masks, shifts)
-    )
+    lowest_bit = [log_length - sum(log_lengths[:i + 1]) for i in range(n_levels)]
+    sizeof_one_entry = sum((1 << log_lengths[i]) * sizeof_pointer for i in range(n_levels - 1)) + (1 << log_lengths[last_level]) * sizeof_value
 
     # Name, but formatted for a type name
     camel_case = "".join(part.capitalize() for part in name.split())
@@ -62,19 +39,19 @@ def multilevel_table(
         "#include <stdint.h>",
         "",
         f"enum _{camel_case}Flag {{ _{upper}_UNALLOCATED = 0, _{upper}_WAITING }};",
-        f"struct _{camel_case}{last_level}Struct {{ _Atomic({value_type}) array [{lengths[last_level]}]; }};",
+        f"struct _{camel_case}{last_level}Struct {{ _Atomic({value_type}) array [{1 << log_lengths[last_level]}]; }};",
         *[
             "\n".join([
-                f"union _{camel_case}{i}Union {{ struct _{camel_case}{i+1}Struct* ptr; enum _{camel_case}Flag flag; }};",
-                f"struct _{camel_case}{i}Struct {{ _Atomic(union _{camel_case}{i}Union) array [{lengths[i]}]; }};",
+                f"union _{camel_case}{i}Union {{ struct _{camel_case}{i+1}Struct* _Nullable ptr; enum _{camel_case}Flag flag; }};",
+                f"struct _{camel_case}{i}Struct {{ _Atomic(union _{camel_case}{i}Union) array [{1 << log_lengths[i]}]; }};",
             ])
             for i in range(last_level - 1, -1, -1)
         ],
         f"struct {camel_case} {{ struct _{camel_case}0Struct inner; }};",
         "",
-        f"{fn_attrs} {value_type} {snakecase}_get_or_default(const struct {camel_case}* {snakecase}, {index_type} index, {value_type} def);",
+        f"{fn_attrs} const _Atomic({value_type})* _Nullable {snakecase}_address_of_weak(const struct {camel_case}* _Nonnull {snakecase}, {index_type} index);",
         "",
-        f"{fn_attrs} bool {snakecase}_compare_exchange(struct {camel_case}* {snakecase}, {index_type} index, {value_type} expected, {value_type} desired);",
+        f"{fn_attrs} _Atomic({value_type})* _Nonnull {snakecase}_address_of_strong(struct {camel_case}* _Nonnull {snakecase}, {index_type} index);",
         "",
         f"// sizeof table with one entry = {sizeof_one_entry / 1024:.1f}KiB = {sizeof_one_entry / 1024 / 1024:.1f}MiB",
     ]))
@@ -84,55 +61,50 @@ def multilevel_table(
         "#include \"../src/debug_logging.h\"",
         f"#include \"{c_header}\"",
         "",
-        *[
-            f"#define _{upper}{i}_MASK 0x{mask:0{log_length_rounded // 4}x}"
-            for i, mask in enumerate(masks)
-        ],
-        *[
-            f"#define _{upper}{i}_SHIFT {shifts[i]}"
-            for i, mask in enumerate(masks)
-        ],
+        "#define _BITS(value, low, length) (((((1L << length) - 1L) << low) & value) >> low)",
         "",
-        f"{value_type} {snakecase}_get_or_default(const struct {camel_case}* {snakecase}, {index_type} index, {value_type} def) {{",
+        f"const _Atomic({value_type})* _Nullable {snakecase}_address_of_weak(const struct {camel_case}* _Nonnull {snakecase}, {index_type} index) {{",
+        "    _Static_assert(ATOMIC_BOOL_LOCK_FREE, \"\");",
+        "    _Static_assert(ATOMIC_POINTER_LOCK_FREE, \"\");",
         f"  const struct _{camel_case}0Struct* struct0 = &{snakecase}->inner;",
-        "  size_t subindex;",
+        "",
         *itertools.chain.from_iterable([
             [
-                f"  subindex = (index & _{upper}{i}_MASK) >> _{upper}{i}_SHIFT;",
-                f"  const union _{camel_case}{i}Union union{i} = atomic_load(&*struct{i}.array[subindex]);",
-                f"  if (union{i}.flag == _{upper}_UNALLOCATED || union{i}.flag == _{upper}_WAITING) {{ return def; }}",
+                f"  const union _{camel_case}{i}Union union{i} = atomic_load(&struct{i}->array[_BITS(index, {lowest_bit[i]}L, {log_lengths[i]}L)]);",
+                f"  if (union{i}.flag == _{upper}_UNALLOCATED || union{i}.flag == _{upper}_WAITING) {{ return NULL; }}",
                 f"  const struct _{camel_case}{i+1}Struct* struct{i+1} = union{i}.ptr;",
                 "",
             ]
             for i in range(last_level)
         ]),
-        f"  subindex = (index & _{upper}{last_level}_MASK) >> _{upper}{last_level}_SHIFT;",
-        f"  {value_type} ret = atomic_load(&*struct{last_level}.array[subindex]);",
-        "  return ret;",
+        f"  return &struct{last_level}->array[_BITS(index, {lowest_bit[last_level]}L, {log_lengths[last_level]}L)];",
         "}",
         "",
-        f"bool {snakecase}_compare_exchange(struct {camel_case}* {snakecase}, {index_type} index, {value_type} expected, {value_type} desired) {{",
-        f"  struct _{camel_case}0Struct* struct0 = &{snakecase}->inner;"
-        "  size_t subindex;",
+        f"_Atomic({value_type})* _Nonnull {snakecase}_address_of_strong(struct {camel_case}* _Nonnull {snakecase}, {index_type} index) {{",
+        f"  struct _{camel_case}0Struct* struct0 = &{snakecase}->inner;",
+        "",
         *itertools.chain.from_iterable([
             [
-                f"  subindex = (index & _{upper}{i}_MASK) >> _{upper}{i}_SHIFT;",
                 f"  union _{camel_case}{i}Union expected{i} = {{ .flag = _{upper}_UNALLOCATED }};",
-                f"  union _{camel_case}{i}Union desired{i} = {{ .flag = _{upper}_WAITING }};",
+                f"  union _{camel_case}{i}Union waiting{i} = {{ .flag = _{upper}_WAITING }};",
                 f"  struct _{camel_case}{i+1}Struct* struct{i+1};",
-                f"  if (atomic_compare_exchange_strong(&struct{i}->array[subindex], &expected{i}, desired{i})) {{",
+                f"  if (atomic_compare_exchange_strong(&struct{i}->array[_BITS(index, {lowest_bit[i]}L, {log_lengths[i]}L)], &expected{i}, waiting{i})) {{",
                 f"    struct{i+1} = expected{i}.ptr = EXPECT_NONNULL(calloc(sizeof(struct _{camel_case}{i+1}Struct), 1));",
-                f"    atomic_store(&struct{i}->array[subindex], expected{i});",
+                f"    atomic_store(&struct{i}->array[_BITS(index, {lowest_bit[i]}L, {log_lengths[i]}L)], expected{i});",
                 "  } else {",
-                f"    while (expected{i}.flag == 1) {{ expected{i} = atomic_load(&struct{i}->array[subindex]); }}",
+                "    size_t i = 0;"
+                f"    while (expected{i}.flag == _{upper}_WAITING) {{",
+                f"      expected{i} = atomic_load(&struct{i}->array[_BITS(index, {lowest_bit[i]}L, {log_lengths[i]}L)]);",
+                "      if (i != 0 && i % 5 == 0) { WARNING(\"Spinning %d times\", ++i); }",
+                "      i++;",
+                "    }",
                 f"    struct{i+1} = expected{i}.ptr;",
-                "}",
+                "  }",
                 "",
             ]
             for i in range(last_level)
         ]),
-        f"  subindex = (index & _{upper}{last_level}_MASK) >> _{upper}{last_level}_SHIFT;",
-        f"  return atomic_compare_exchange_strong(&struct{last_level}->array[subindex], &expected, desired);",
+        f"  return &struct{last_level}->array[_BITS(index, {lowest_bit[last_level]}L, {log_lengths[last_level]}L)];",
         "}",
     ]))
 
--- libprobe/src/global_state.c
+++ libprobe/src/global_state.c
@@ -109,18 +109,10 @@ pid_t get_pid_safe() { return probe_libc_getpid(); }
 
 static struct InodeTable __read_inodes;
 static struct InodeTable __copied_or_overwritten_inodes;
-static inline void init_tables() {
-    ASSERTF(!inode_table_is_init(&__read_inodes), "");
-    ASSERTF(!inode_table_is_init(&__copied_or_overwritten_inodes), "");
-    inode_table_init(&__read_inodes);
-    inode_table_init(&__copied_or_overwritten_inodes);
-}
 struct InodeTable* get_read_inodes() {
-    ASSERTF(inode_table_is_init(&__read_inodes), "");
     return &__read_inodes;
 }
 struct InodeTable* get_copied_or_overwritten_inodes() {
-    ASSERTF(inode_table_is_init(&__copied_or_overwritten_inodes), "");
     return &__copied_or_overwritten_inodes;
 }
 
@@ -428,7 +420,6 @@ void init_proc() {
     check_function_pointers();
     init_pid(false);
     init_probe_dir();
-    init_tables();
     init_process_tree_context();
     init_process_context();
     create_epoch_dir();
