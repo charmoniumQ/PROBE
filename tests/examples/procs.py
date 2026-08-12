fdf9a5dcc28fcff82ba84597b4c850fe9d2f500c
Samuel Grayson <sam@samgrayson.me>
2026-08-12 17:49:55 -0500

    Fixes, but commented out

--- tests/examples/procs.py
+++ tests/examples/procs.py
@@ -1,10 +1,20 @@
 import multiprocessing
 import threading
+import pathlib
+
+
+def f1():
+    pathlib.Path("flake.nix").read_text()
+
+
+def f2():
+    pathlib.Path("flake.lock").read_text()
+
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=print, args=("hello from thread",))
+    thread = threading.Thread(target=f1, args=("hello from thread",))
     thread.start()
-    proc = multiprocessing.Process(target=print, args=("hello from proc",))
+    proc = multiprocessing.Process(target=f2, args=("hello from proc",))
     proc.start()
     proc.join()
     thread.join()
